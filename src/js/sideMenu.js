0772efbade25ce61a523b850c45fabbdf290ea4b
Jeandre Melaria <J.melaria@hotmail.com>
2018-02-16 19:21:18 +0100

    Move sidemenu javascript code to external sideMenu.js file

---
+++ src/js/sideMenu.js
@@ -0,0 +1,24 @@
+document.getElementById('menuToggle').addEventListener('click', openSideMenu);
+document.getElementById('closeIcon').addEventListener('click', closeSideMenu);
+
+function openSideMenu() {
+    var w = window.innerWidth;
+    var sideMenu = document.getElementById('sideMenu');
+
+    if (w > 575 && w < 768) {
+        sideMenu.style.width = '14.5rem';
+
+    } else if (w > 767 && w < 992) {
+        sideMenu.style.width = '17rem';
+
+    } else if (w > 991 && w < 1200) {
+        sideMenu.style.width = '21.7rem';
+
+    } else {
+        sideMenu.style.width = '26rem';
+    }
+}
+
+function closeSideMenu() {
+    document.getElementById('sideMenu').style.width = '0';
+}
