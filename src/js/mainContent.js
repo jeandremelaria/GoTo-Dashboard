e3d2db750131a2b1cad20f5abea2df548eecf5be
Jeandre Melaria <J.melaria@hotmail.com>
2018-02-27 10:11:05 +0100

    Move sidemenu js code to sideMenu.js

---
+++ src/js/mainContent.js
@@ -0,0 +1,29 @@
+//  // Main content 
+
+//  // Events Listener
+//  document.getElementById('sideMenuDashboard').addEventListener('click', getDashboard);
+// //  document.getElementById('sideMenuProfile').addEventListener('click', getProfile);
+//  document.getElementById('sideMenuCalendar').addEventListener('click', getCalendar);
+
+//  // Variables
+//  var dashboard = document.getElementById('dashboard');
+//  var profile = document.getElementById('profile');
+//  //  var calendar = document.getElementById('calendar');
+
+//  function getDashboard() {
+//      profile.style.display = 'none';
+//      //  calendar.style.display = 'none';
+//      dashboard.style.display = 'block';
+//  }
+
+//  function getProfile() {
+//      dashboard.style.display = 'none';
+//      //  calendar.style.display = 'none';
+//      profile.style.display = 'block';
+//  }
+
+//  function getCalendar() {
+//      dashboard.style.display = 'none';
+//      profile.style.display = 'none';
+//      //  calendar.style.display = 'block';
+//  }
